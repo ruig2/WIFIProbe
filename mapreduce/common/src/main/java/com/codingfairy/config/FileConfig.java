925072f1af926388400a6c8a3ab4a7baa68647c0
Rui Guo <ruig2@uci.edu>
2020-03-08 16:53:18 -0700

    fix path issue

--- mapreduce/common/src/main/java/com/codingfairy/config/FileConfig.java
+++ mapreduce/common/src/main/java/com/codingfairy/config/FileConfig.java
@@ -8,8 +8,8 @@ public class FileConfig {
     /**上传的文件进入{@link FileConfig#data}
      * merged过的文件进入{@link FileConfig#data}
      */
-    public static final String data = "/input";
-    public static final String classify = "/wifi/classify";
-    public static final String statistic = "/wifi/statistic";
+    public static final String data = "input";
+    public static final String classify = "wifi/classify";
+    public static final String statistic = "wifi/statistic";
 
 }
